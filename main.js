611f26aa1457c5b493ed25224fef9fe64fda2f98
Hossain K. M. <awarehossain@gmail.com>
2024-03-05 01:20:48 +0600

    cmt-✅

--- main.js
+++ main.js
@@ -12,3 +12,16 @@ var swiper = new Swiper(".home", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+const menu = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
